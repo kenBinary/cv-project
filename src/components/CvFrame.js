872e4df46228ad4086706e184ca7dbbef67bf77e
thennek0 <kennethbecaro0@gmail.com>
2023-06-20 15:15:51 +0800

    add components for cv page

--- src/components/CvFrame.js
+++ src/components/CvFrame.js
@@ -1,9 +1,17 @@
+import Description from "./Description";
+import Education from "./Education";
+import Name from "./NameSection";
+import Skills from "./Skills";
+import WorkExperience from "./WorkExperience";
+
 function CvPage() {
     return (
         <main>
-            <h1>
-                Hello World, my old friend
-            </h1>
+            <Name></Name>
+            <Description></Description>
+            <WorkExperience></WorkExperience>
+            <Education></Education>
+            <Skills></Skills>
         </main>
     );
 }
---
+++ src/components/Description.js
@@ -0,0 +1,7 @@
+export default function Description(params) {
+    return (
+        <h1>
+            Description
+        </h1>
+    );
+}
